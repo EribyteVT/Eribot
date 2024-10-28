fa6f2555c6ddf48051436d1750d1d8d047034dcf
amy <exhqdev@proton.me>
2024-10-29 01:38:06 +0330

    girls kissing

--- cogs/MiscCommands.py
+++ cogs/MiscCommands.py
@@ -4,12 +4,41 @@ import discord
 from utils.utils import isAdmin
 from discord import app_commands
 import random
+import requests
 from typing import Optional
 
 class MiscCommands(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
         self._last_member = None
+
+    @app_commands.command(name = "girlskissing", description="you see daughter, when two girls like eachother very much")
+    async def girlkissing(self, interaction: discord.Interaction):
+        await interaction.response.defer(thinking=True)
+        #do not remove `rating:g`. the server will return porn lmao
+        resp = requests.get("https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&tags=2girls%20kiss%20rating:g")
+        if resp.status_code == 200:
+            pages = resp.json()["@attributes"]['count'] // 100
+            resppage = requests.get(f"https://gelbooru.com/index.php?page=dapi&s=post&q=index&pid={random.randint(1,pages)}&json=1&tags=2girls%20kiss%20rating:g")
+            if resppage.status_code == 200:
+                post = resppage.json()["post"][random.randint(0, len(resppage.json()["post"]))]
+                embed = discord.Embed(title="post #7944344",
+                      colour=0x00b0f4)
+
+                embed.set_author(name="girls kissing")
+                embed.add_field(name="uploaded by",value=post["owner"])
+                embed.add_field(name="tags:", value=" ".join(post["tags"].split(" ")[:10]))
+                embed.add_field(name="source", value=post["source"])
+
+                embed.set_image(url=f"https://proxy.mono.exhq.dev/_/plain/{post["file_url"]}")
+
+                await interaction.followup.send(embed=embed)
+            else:
+                await interaction.followup.send(content = "gelbooru shat itself lmaoooo")
+        else:
+            await interaction.followup.send(content = "gelbooru shat itself lmaoooo")
+
+
 
     @app_commands.command(name = "compliment", description="says something nice about you")
     async def compliment(self, interaction: discord.Interaction):
@@ -21,11 +50,6 @@ class MiscCommands(commands.Cog):
     async def sync_prefix(self, ctx: commands.Context, guild_id_to_sync: Optional[str] = None):
         self.client.tree.copy_global_to(guild=ctx.guild)
         commands = await self.client.tree.sync(guild=ctx.guild)
-
-        for command in commands:
-            message += command.name +"\n"
-            
-        await ctx.reply(content = "Synced the following commands:\n"+message)
 
 
     @app_commands.command(name = "sync", description="Syncs the tree (admin only)")
